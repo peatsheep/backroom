8247e51dcde4763d99aa04f44de2e43c06a6e9af
Will Neville <william@neville.com>
2018-12-12 20:57:36 -0500

    updated service to read from db

--- service.py
+++ service.py
@@ -1,4 +1,14 @@
 import sqlite3
+from flask import g
+
+DATABASE = 'data/bloodborne.db'
+
+def get_db():
+  db = getattr(g, '_database', None)
+  if db is None:
+    db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = dict_factory
+  return db
 
 def dict_factory(cursor, row):
     d = {}
@@ -6,107 +16,10 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-def db_connect():
-  conn = sqlite3.connect('data/bloodborne.db')
-  conn.row_factory = dict_factory
-  return conn.cursor()
-
 def get_weapons():
-  return [
-    {
-      "name": "Amygdalan Arm",
-      "str_req": 17,
-      "skl_req": 9,
-      "blt_req": 0,
-      "arc_req": 0
-    },
-    {
-      "name": "Beast Claw",
-      "str_req": 14,
-      "skl_req": 12,
-      "blt_req": 0,
-      "arc_req": 0
-    },
-    {
-      "name": "Beasthunter Saif",
-      "str_req": 9,
-      "skl_req": 11,
-      "blt_req": 0,
-      "arc_req": 0
-    },
-    {
-      "name": "Beast Cutter",
-      "str_req": 11,
-      "skl_req": 9,
-      "blt_req": 0,
-      "arc_req": 0
-    }
-  ]
+  pass
 
 def get_armor():
-  return [
-    {
-      "name": "Grey Wolf Cap",
-      "type": "Head",
-      "set": "Ashen Hunter",
-      "phys_def": 50,
-      "blunt_def": 40,
-      "thrust_def": 40,
-      "blood_def": 50,
-      "arc_def": 20,
-      "fire_def": 50,
-      "bolt_def": 30,
-      "slow_psn_res": 9,
-      "rapid_psn_res": 12,
-      "frenzy_res": 19,
-      "beasthood": 37
-    },
-    {
-      "name": "Ashen Hunter Garb",
-      "type": "Chest",
-      "set": "Ashen Hunter",
-      "phys_def": 100,
-      "blunt_def": 110,
-      "thrust_def": 80,
-      "blood_def": 110,
-      "arc_def": 70,
-      "fire_def": 50,
-      "bolt_def": 70,
-      "slow_psn_res": 25,
-      "rapid_psn_res": 45,
-      "frenzy_res": 60,
-      "beasthood": 70
-    },
-    {
-      "name": "Ashen Hunter Gloves",
-      "type": "Hand",
-      "set": "Ashen Hunter",
-      "phys_def": 40,
-      "blunt_def": 60,
-      "thrust_def": 50,
-      "blood_def": 40,
-      "arc_def": 50,
-      "fire_def": 40,
-      "bolt_def": 50,
-      "slow_psn_res": 12,
-      "rapid_psn_res": 24,
-      "frenzy_res": 28,
-      "beasthood": 32
-    },
-    {
-      "name": "Ashen Hunter Trousers",
-      "type": "Leg",
-      "set": "Ashen Hunter",
-      "phys_def": 60,
-      "blunt_def": 50,
-      "thrust_def": 50,
-      "blood_def": 60,
-      "arc_def": 50,
-      "fire_def": 50,
-      "bolt_def": 60,
-      "slow_psn_res": 15,
-      "rapid_psn_res": 26,
-      "frenzy_res": 30,
-      "beasthood": 37
-    }
-  ]
+  cur = get_db().cursor()
+  cur.execute('SELECT * FROM attire;')
+  return cur.fetchall()
