f3b85ea56f7193822a34ff66a231bc9ef9ddaf5c
Will Neville <william@neville.com>
2018-12-22 16:08:51 -0500

    re-re-re-reboot

---
+++ supply-closet/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_mapping(
+    SECRET_KEY='dev', #should be overriden with randon val at deploy
+    DATABASE=os.path.join(app.instance_path, 'bloodborne.sqlite'),
+  )
+
+  # Determining whether to load in default config or passed in test config
+  if test_config is None:
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    app.config.from_mapping(test_config)
+  
+  # Ensure instance 
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+
+  return app
